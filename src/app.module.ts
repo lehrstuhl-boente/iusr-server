05b5a5e37883c0d8a3199669ae5ad26fb1f6a227
Atomis14 <michelsabbatini@outlook.com>
2023-02-13 08:55:21 +0100

    create prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CourseModule } from './course/course.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CourseModule],
+  imports: [AuthModule, UserModule, CourseModule, PrismaModule],
 })
 export class AppModule {}
