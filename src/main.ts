0d1ad3c975e413ed4495fb92c10f02f6226c565b
Atomis14 <michelsabbatini@outlook.com>
2023-03-30 10:00:54 +0200

    implement custom validation return type

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
-import { ValidationPipe } from '@nestjs/common';
+import {
+  BadRequestException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // adjust exception factory so that not only error messages but also field keys are returned
+      exceptionFactory: (errors: ValidationError[]) => {
+        let newErrors = [];
+        errors.forEach((error) => {
+          newErrors.push({
+            field: error.property,
+            errors: Object.values(error.constraints),
+          });
+        });
+        return new BadRequestException(newErrors);
+      },
+    }),
+  );
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(3001);
